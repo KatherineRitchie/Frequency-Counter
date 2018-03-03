bb0f4e151f4df88a50930ebd7b2b488bcdccceef
Katherine Ritchie <ritchie5@illinois.edu>
2018-03-03 14:18:06 -0600

    Test Commit

---
+++ cpp/Main.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Katherine Ritchie on 3/2/18.
+//
+
+#include "Main.h"
+
+// 'Hello World!' program
+
+#include <iostream>
+
+int main();
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
