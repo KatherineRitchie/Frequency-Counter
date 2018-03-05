4ecc2630183edc3bbfd5cf637e6bdd80729fc2d4
Katherine Ritchie <ritchie5@illinois.edu>
2018-03-05 15:20:35 -0600

    arrToCountMap() function working

--- cpp/src/Main.cpp
+++ cpp/src/Main.cpp
@@ -15,8 +15,8 @@ using namespace std;
 using namespace std;
 
 int main(int argc, char* argv[]);
-string analyseWordFrequency(char* argv[], int numElements);
-map<string, int> arrToCountMap(char* argv[]);
+string analyseWordFrequency(int argc, char* argv[], int numElements);
+map<string, int> arrToCountMap(int argc, char* argv[]);
 map<string, int> findMostPopular(map<string, int> unsortedMap, int numElements);
 string findMaxKeyInMap(map<string, int> unsortedMap);
 string mapToString(map<string, int> mostSeenMap);
@@ -31,26 +31,29 @@ int main(int argc, char* argv[])
         std::cout << argv[i] << std::endl;
     }
 
-    std::cout << analyseWordFrequency(argv, 10) << std::endl;
+    std::cout << analyseWordFrequency(argc, argv, 10) << std::endl;
     return 0;
 
 }
 
-string analyseWordFrequency(char* argv[], int numElements) {
+string analyseWordFrequency(int argc, char* argv[], int numElements) {
     string textString = "dfghjk";
-    map<string, int> unsortedCountMap = arrToCountMap(argv);
+    map<string, int> unsortedCountMap = arrToCountMap(argc, argv);
     map<string, int> mostPopularMap = findMostPopular(unsortedCountMap, numElements);
     string resultsString = mapToString(mostPopularMap);
     return resultsString;
 }
 
-map<string, int> arrToCountMap(char* argv[]) {
-    //std::istringstream textStringStream(textString);
-    //std::vector<string> textVector ((std::istream_iterator<std::string>(textStringStream)),
-                                    //std::istream_iterator<std::string>());
-
+map<string, int> arrToCountMap(int argc, char* argv[]) {
     map<string, int> resultMap;
-    resultMap.insert(std::pair<string, int>("test string", 5));
+    for (int i = 0; i < argc; i++) {
+        if (resultMap.count(argv[i])) {
+            int currentVal = resultMap.at(argv[i]);
+            resultMap.at(argv[i]) = currentVal + 1;
+        } else {
+            resultMap.emplace(argv[i], 1);
+        }
+    }
     return resultMap;
 }
 
